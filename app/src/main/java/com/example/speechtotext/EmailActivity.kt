5fe09618dafb3c5fc769a3c9281cb5ac69717d46
DanielBuenoNavarroo <dabue410@alu.centroafuera.es>
2023-12-01 21:52:12 +0100

    Nueva logica en EmailActivity, creados insertar_mail.xml, insertar_message.xml y insertar_subject.xml

--- app/src/main/java/com/example/speechtotext/EmailActivity.kt
+++ app/src/main/java/com/example/speechtotext/EmailActivity.kt
@@ -1,32 +1,60 @@
 package com.example.speechtotext
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import androidx.core.text.set
-import javax.security.auth.Subject
+import androidx.appcompat.app.AppCompatActivity
 
 class EmailActivity : AppCompatActivity() {
-    private lateinit var textTo : TextView
-    private lateinit var textSubject : TextView
-    private lateinit var editTextMessage : TextView
-    private lateinit var btnSend : Button
-    private lateinit var msg : String
+    private lateinit var textTo: TextView
+    private lateinit var textSubject: TextView
+    private lateinit var editTextMessage: TextView
+    private lateinit var btnSend: Button
+    private var cont = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        iniciarInsertarMail()
+    }
+
+    private fun iniciarInsertarMail(){
+        setContentView(R.layout.insertar_mail)
+
+        val btnSiguiente : Button = findViewById(R.id.btnSiguiente)
+        btnSiguiente.setOnClickListener {
+            iniciarInsertarSubject()
+        }
+    }
+
+    private fun iniciarInsertarSubject(){
+        setContentView(R.layout.insertar_subject)
+
+        val btnSiguiente : Button = findViewById(R.id.btnSiguiente)
+        btnSiguiente.setOnClickListener {
+            iniciarInsertarMessage()
+        }
+    }
+
+    private fun iniciarInsertarMessage(){
+        setContentView(R.layout.insertar_message)
+
+        val btnSiguiente : Button = findViewById(R.id.btnSiguiente)
+        btnSiguiente.setOnClickListener {
+            emailActivity()
+        }
+
+    }
+
+    private fun emailActivity(){
         setContentView(R.layout.activity_email)
 
         textTo = findViewById(R.id.textEmailTo)
         textSubject = findViewById(R.id.textEmailSubject)
         editTextMessage = findViewById(R.id.textEmailMessage)
-
-        msg = intent.getStringExtra("MENSAJE").toString()
-        editTextMessage.text = msg
-
+        editTextMessage.text = intent.getStringExtra("MENSAJE")
         btnSend = findViewById(R.id.emailButtonSend)
+
         btnSend.setOnClickListener {
             sendMail()
         }
@@ -38,7 +66,7 @@ class EmailActivity : AppCompatActivity() {
         val message = editTextMessage.text.toString()
 
         intent = Intent(Intent.ACTION_SEND)
-        intent.putExtra(Intent.EXTRA_EMAIL, mail)
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mail))
         intent.putExtra(Intent.EXTRA_SUBJECT, subject)
         intent.putExtra(Intent.EXTRA_TEXT, message)
 
--- app/src/main/java/com/example/speechtotext/MainActivity.kt
+++ app/src/main/java/com/example/speechtotext/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.widget.Button
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         textView = findViewById(R.id.text)
         val speak: ImageView = findViewById(R.id.speak)
 
@@ -41,9 +42,14 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             REQ_CODE -> {
                 if (resultCode == RESULT_OK && data != null) {
-                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    var result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).toString()
+                    result = result.substring(1, result.length - 1) // Se hace un substring porque viene en formato [texto] y le queremos quitar los corchetes
+
+                    // ESTO ES PARA VER SI EL MENSAJE SE RECOGE CORRECTAMENTE, ELIMINAR CUANDO ENTREGUEMOS
+                    Log.i("Mensaje", result) // filtrar por Mensaje en el logcat
+
                     intent = Intent(this@MainActivity, EmailActivity::class.java)
-                    intent.putExtra("MENSAJE", result.toString())
+                    intent.putExtra("MENSAJE", result)
                     startActivity(intent)
                 }
             }
